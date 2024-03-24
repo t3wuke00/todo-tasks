625096aa52c8286f5c7fc9902b0431def81e9fe8
“t3wuke00” <t3wuke00@students.oamk.fi>
2024-03-24 18:14:35 +0200

    todo-tasks codes

---
+++ todo1/js/index.js
@@ -0,0 +1,86 @@
+const BACKEND_ROOT_URL = 'http://127.0.0.1:3001';
+import { Todos } from './class/Todos.js'
+
+const todos = new Todos(BACKEND_ROOT_URL)
+
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+
+
+input.disabled = true
+
+
+const renderTask =(task) =>{
+    const li = document.createElement('li')
+    li.setAttribute('class','list-group-item')
+    li.setAttribute('data-key',task.getId().toString())
+    li.innerHTML = task.getText()
+    renderSpan(li,task.getText())
+    renderLink(li,task.getId())
+    list.append(li)
+}
+
+const renderSpan = (li, text) => {
+    const span = li.appendChild(document.createElement('span'))
+    span.innerHTML = text
+}
+
+const renderLink = (li,id) => {
+    const a = li.appendChild(document.createElement('a'))
+    a.innerHTML = '<i class="bi bi-trash"></i>'
+    a.setAttribute('style','float: right')
+    a.addEventListener('click',(event) => {
+        todos.removeTask(id).then((remove_id) =>{
+            const li_to_remove = document.querySelector(`[data-key='${remove_id}']`)
+            if(li_to_remove) {
+                list.removeChild(li_to_remove)
+            }
+        }).catch((error) => {
+            alert(error)
+        })
+    })
+}
+
+const getTask = () => {
+    todos.getTask().then((tasks)=> {
+        tasks.forEach(task => {
+            renderTask(task)  
+        });
+        input.disabled = false
+    }).catch((error)=> {
+        alert(error)
+    })
+   }
+
+
+const saveTask = async (task) => {
+    try {
+        const json = JSON.stringify({description: task})
+        const response = await fetch(BACKEND_ROOT_URL + '/new', {
+            method: 'post',
+            headers: {
+                'Content-Type':'application/json'
+            }
+        })
+        return response.json()
+    }catch (error){
+    alert("Error saving task " + error.message)
+    }
+}
+
+input.addEventListener('keypress', (event) =>{
+    if (event.key === 'Enter'){
+        event.preventDefault();
+        const task = input.value.trim()
+        if (task !== ''){
+            todos.addTask(task).then((task)=>{
+                renderTask(task)
+                input.value = ''
+                input.focus()
+            })
+    
+        }
+    }
+})
+
+getTask()
---
+++ todo2/server/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const {todoRouter} = require('./routes/todo')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use('/' ,todoRouter)
+
+
+const port = process.env.PORT
+
+app.listen(port);
